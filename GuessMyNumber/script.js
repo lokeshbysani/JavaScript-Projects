0e64ef02025de740f506aedd27f82a38c55096ba
Lokesh Bysani <bysani3497@gmail.com>
2022-04-24 12:42:47 +0000

    AddsEventListener

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//Add Event listener for check button
+
+document.querySelector('.check').addEventListener('click',function(){
+    const userGuess = Number(document.querySelector('.guess').value);
+    console.log(userGuess,typeof userGuess)
+
+    if(!userGuess){
+        document.querySelector('.message').textContent = 'No Number Selected.'
+    }
+
+})
